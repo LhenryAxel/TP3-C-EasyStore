bd62e135b10349ebc271bd1f076228278530fd06
sylvain <sylvainsoudais23@gmail.com>
2023-12-07 09:21:40 +0100

    client

--- Client.h
+++ Client.h
@@ -6,8 +6,8 @@ class Client{
         std::string _Id;
         std::vector<panier> _panier;
     public:
-        Client(st::string Nom, std::string Prenom, std::string Id,std::vector<panier> _panier);
-        
+        Client(std::string Nom, std::string Prenom, std::string Id,std::vector<panier> _panier);
+
 
 
 
