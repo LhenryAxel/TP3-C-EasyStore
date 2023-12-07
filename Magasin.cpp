281d1e9e85f5b0a8f5a0650c35dfb873a17a967e
LhenryAxel <axelnolan.lhenry@gmail.com>
2023-12-07 09:20:17 +0100

    ajout de methode ajoute et affiche produit

--- Magasin.cpp
+++ Magasin.cpp
@@ -1,5 +1,27 @@
 #include "magasin.h"
 
 Magasin::Magasin() {
+    //init
+    
+}
+
+void Magasin::ajouterProduit(const Produit& produit) {
+    _products.push_back(produit);
+}
+
+void Magasin::afficherProduits() const {
+    std::cout << "Liste des produits dans le magasin :\n";
+    for (const auto& produit : _products) {
+        std::cout << produit << "\n";
+    }
+}
+
+void Magasin::afficherProduitParNom(const std::string& nom) const {
+    std::cout << "Recherche du produit par nom : " << nom << "\n";
+
+}
+
+void Magasin::mettreAJourQuantiteProduit(const std::string& nom, int nouvelleQuantite) {
+    std::cout << "Mise à jour de la quantité du produit : " << nom << "\n";
 
 }
--- Magasin.h
+++ Magasin.h
@@ -8,7 +8,23 @@
 class Magasin {
 public:
     Magasin();
-    // ... (autres fonctions membres nĂ©cessaires)
+    void ajouterProduit(const Produit& produit);
+    void afficherProduits() const;
+    void afficherProduitParNom(const std::string& nom) const;
+    void mettreAJourQuantiteProduit(const std::string& nom, int nouvelleQuantite);
+    
+    /*
+    void ajouterClient(const Client& client);
+    void afficherClients() const;
+    void afficherClientParNomOuId(int id, const std::string& nom) const;
+    void ajouterProduitAuPanier(int idClient, const Produit& produit, int quantite);
+    void supprimerProduitDuPanier(int idClient, const Produit& produit);
+    void modifierQuantiteProduitPanier(int idClient, const Produit& produit, int nouvelleQuantite);
+
+    void validerCommande(int idCommande);
+    void mettreAJourStatutCommande(int idCommande, const std::string& nouveauStatut);
+    void afficherToutesCommandes() const;
+    void afficherCommandesClient(int idClient) const;*/
 
 private:
     std::vector<Produit> _products;
